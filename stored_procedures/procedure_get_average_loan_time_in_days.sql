1017e73bd4c76af57abb6288e707f9e39b32696e
patrykiciak <patryk.iciak@gmail.com>
2021-05-24 21:24:43 +0200

    haha

--- stored_procedures/procedure_get_average_loan_time_in_days.sql
+++ stored_procedures/procedure_get_average_loan_time_in_days.sql
@@ -4,6 +4,5 @@ as
 begin
     SELECT AVG(DATEDIFF(DAY, loaned_at, returned_at)) AS average_loan_length_in_days
     FROM loan l
-             JOIN book b ON l.book_isbn = b.isbn
     WHERE l.returned_at IS NOT NULL;
 end
